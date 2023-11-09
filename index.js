7513285ff16df4d122fb63d14352aefeeb65d7ff
fosemberg <fosemberg@gmail.com>
2023-11-09 13:44:19 +0300

    update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        https://m.youtube.com/watch?v=lUDo3TW6DHw
+// @match        https://m.youtube.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
