d2c7cdbcb80db23513b6ca1f2ef3ce8a5846b6b2
fosemberg <fosemberg@gmail.com>
2023-11-09 13:44:51 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         youtube audio
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://m.youtube.com
